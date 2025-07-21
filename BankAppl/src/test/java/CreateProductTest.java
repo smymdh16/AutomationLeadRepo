5a8f36d9e25295a7df56d2663e6442532a8b9418
smymdh16 <smymdh16@gmail.com>
2025-07-21 12:34:25 +0530

    Modified file CreateProductTest added new statement

--- BankAppl/src/test/java/CreateProductTest.java
+++ BankAppl/src/test/java/CreateProductTest.java
@@ -4,6 +4,9 @@ public class CreateProductTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("create Test class for product test");
+
+
+System.out.println("I am modifiying the script :modified by testeng2");
 	}
 
 }
