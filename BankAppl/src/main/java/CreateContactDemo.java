777af95d6ef75bfc8f1a3ba08f20032fbce35768
smymdh16 <smymdh16@gmail.com>
2025-07-21 11:10:37 +0530

    Modified ContactPage added few more webelements

--- BankAppl/src/main/java/CreateContactDemo.java
+++ BankAppl/src/main/java/CreateContactDemo.java
@@ -4,6 +4,11 @@ public class CreateContactDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Created pom class for contact page");
+
+System.out.println("Created pom class for create contact page ");
+
+System.out.println("created pom class for contact info page");
+//>message represnts the page is getting modified it is not present in github
 	}
 
 }
