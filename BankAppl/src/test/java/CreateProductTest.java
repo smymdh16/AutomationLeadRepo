896d96f6ea7429398fc7877dfeca110a18bb4124
smymdh16 <smymdh16@gmail.com>
2025-07-21 12:49:12 +0530

    modified by sam test engineer createProductTest

--- BankAppl/src/test/java/CreateProductTest.java
+++ BankAppl/src/test/java/CreateProductTest.java
@@ -4,6 +4,8 @@ public class CreateProductTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("create Test class for product test");
+
+System.out.println("Modified by test engineer sam");
 	}
 
 }
