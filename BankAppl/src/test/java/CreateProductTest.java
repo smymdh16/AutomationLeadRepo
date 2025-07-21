199c441ed3a672208021d8df0abd09ce3c8f97a9
smymdh16 <smymdh16@gmail.com>
2025-07-21 12:44:22 +0530

    Modified added new lines

--- BankAppl/src/test/java/CreateProductTest.java
+++ BankAppl/src/test/java/CreateProductTest.java
@@ -8,6 +8,7 @@ System.out.println("create Test class for product test");
 
 System.out.println("I am modifiying the script :modified by testeng2");
 System.out.println("Hi");
+System.out.println("Hello");
 	}
 
 }
