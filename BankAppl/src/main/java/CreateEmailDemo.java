b8aca42bf9333d530b95f385f842ac6d216a8065
smymdh16 <smymdh16@gmail.com>
2025-07-21 11:23:41 +0530

    added new file EmailPage

---
+++ BankAppl/src/main/java/CreateEmailDemo.java
@@ -0,0 +1,11 @@
+
+public class CreateEmailDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Email created successfully");
+//? this symbol indicates it is not present in local repo as well as global repo
+
+	}
+
+}
