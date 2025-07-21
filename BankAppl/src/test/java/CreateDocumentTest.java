251a871215291969db1f9c5baf2555b2de3b45fc
smymdh16 <smymdh16@gmail.com>
2025-07-21 15:07:38 +0530

    Document page is added

---
+++ BankAppl/src/test/java/CreateDocumentTest.java
@@ -0,0 +1,9 @@
+
+public class CreateDocumentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("create document test script modified by steve");
+	}
+
+}
